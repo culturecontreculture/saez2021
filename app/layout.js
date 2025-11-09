dd6b53bf9fba2270155162153ad3b4c31bf57b27
Mathieu Morelle <mathieu@minedeprod.com>
2025-11-09 19:15:57 +0100

    Migration vers système de remboursement

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Saez 2021 - Demande de remboursement',
-  description: 'Demande de remboursement pour les packs Mélancolie et Symphonie des Siècles',
+  title: 'Saez 2021 - Demande de remboursement des disques',
+  description: 'Demande de remboursement pour les offres Mélancolie et Symphonie des Siècles',
 }
 
 export default function RootLayout({ children }) {
